099d2006cadbecc9596934c5324f70f8ae665e0f
orlandoreynoso <orlandoreynoso01@gmail.com>
2018-04-04 08:10:04 -0600

    changing frame provincial

--- template/cover-provincial.php
+++ template/cover-provincial.php
@@ -1,4 +1,4 @@
-<div class="img-provincial">
+<div class="img-provincial"><!-----Iniciar cuadro provincial --- -->
   <img class="p-logo" src="<?php echo IMAGES.'/provincial.jpg'; ?>" alt="Fray Carlos Antonio Cáceres Pereira">
   <div class="p-mensaje">
     <p>Prior PrFray Carlos Antonio Cáceres Pereira ha sido reelegido como Prior Privincial por los frailes capitulares del III Capítulo Provincial en Guatemala de la Provincia de San Vicente Ferrer en Centroamérica. La elección ha sido confirmada por el Maestro de la Orden, fr. Bruno Cadoré, OP.ivincial</p>
@@ -12,30 +12,14 @@
     </div>
   </div>
 
-</div>
+</div><!------------- fin cuadro provincial - ----------------- -->
 <div class="catequesis">
   <div class="titulo-catequesis">
     <h3>Recursos Pastorales</h3>
   </div>
   <div class="contenido">
-    <div class="lista">
-      <p class="texto">Últimos ingresos:</p>
-      <ul class="listado">
-        <li>Contenido 1</li>
-        <li>Contenido 1</li>
-        <li>Contenido 1</li>
-        <li>Contenido 1</li>
-        <li>Contenido 1</li>
-        <li>Contenido 1</li>
-        <li>Contenido 1</li>
-        <li>Contenido 1</li>
-      </ul>
-    </div>
-    <div class="ingreso">
-      <p class="texto">Recursos completos</p>
-      <a class="recursos-link"href="#">
-        <span>ingresar</span>
-      </a>
-    </div>
+    <!-- p class="texto">Últimos ingresos:</p -->
+    <?php get_template_part( 'template/cover', 'recursos'); ?>
+    <a class="ingresar" href="#">Ingresar para más recursos</a>
   </div>
 </div>
---
+++ template/cover-recursos.php
@@ -0,0 +1,26 @@
+<?php
+
+$id_recursos = 113;
+
+?>
+<?php $the_query = new WP_Query(construir_presencia($id_recursos));  ?>
+<?php while ($the_query-> have_posts()): $the_query-> the_post() ?>
+  <div class="recurso">
+    <a class="pilares-ingreso"  href="<?php the_permalink(); ?>">
+      <div class="thumb">      
+        <?php
+        if(has_post_thumbnail()){
+         the_post_thumbnail('medium');
+        }
+        else{ ?>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/dar-la-vida-por-los-demas.jpg" alt="">
+        <?php  }         ?>
+        
+      </div>
+      <div class="titulo">
+        <!-- i class="fa fa-angle-double-right"></i -->
+        <?php the_title(); ?>
+      </div>
+    </a>
+  </div>
+<?php endwhile; wp_reset_postdata(); ?>
